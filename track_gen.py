82aea0e8996af6d1e65f7ea921eacf484588416a
Rajan Patel <rpatel3001@gmail.com>
2018-03-26 22:42:00 -0400

    git status

--- track_gen.py
+++ track_gen.py
@@ -1,12 +1,13 @@
 """Create a list of X,Y points based on straights and constant radius turns."""
 
 import csv
-from math import sin, cos, tan, atan, radians, inf, pi
+from math import sin, cos, tan, atan, radians, inf, pi, atan2
 
 
-def gen_track(filename):
+def gen_track(filename, dd=1, angle=0, closed=False):
     """Turn a list of turns and straights into a list of X,Y points."""
     tracklist = []
+    # read in the list of track elements
     with open(filename) as csvfile:
         for sec in csv.reader(csvfile):
             if sec[0] == "straight":
@@ -18,9 +19,9 @@ def gen_track(filename):
                                   'angle': float(sec[2])})
 
     points = [(0, 0)]
-    angle = 0 * pi
-    dd = 5
+    # generate points for each track element
     for sec in tracklist:
+        # for straights, use linear interpolation between endpoints
         if sec['type'] == "straight":
             numdiv = round(sec['length'] / dd)
             dlen = sec['length'] / numdiv
@@ -29,6 +30,7 @@ def gen_track(filename):
                 dx = cos(angle) * dlen
                 dy = sin(angle) * dlen
                 points.append((p1[0] + dx, p1[1] + dy))
+        # for turns, calculate points along an arc starting at the current point
         elif sec['type'] == "turn":
             dangle = dd / sec['radius']
             numdiv = abs(round(radians(sec['angle']) / dangle))
@@ -43,21 +45,25 @@ def gen_track(filename):
             cx = p1[0] + sign_corr * cos(cangle) * sec['radius']
             cy = p1[1] + sign_corr * sin(cangle) * sec['radius']
             for i in range(numdiv):
+                cangle += dangle
+                angle = (angle + dangle) % (2 * pi)
                 dx = sign_corr * cos(cangle) * sec['radius']
                 dy = sign_corr * sin(cangle) * sec['radius']
                 points.append((cx - dx, cy - dy))
-                cangle += dangle
-                angle = (angle + dangle) % (2 * pi)
 
-    dist = ((points[-1][0] - points[0][0])**2 +
-            (points[-1][1] - points[0][1])**2)**.5
-    numdiv = round(dist / dd)
-    dlen = dist / numdiv
-    for i in range(numdiv):
-        p1 = points[-1]
-        dx = cos(angle) * dlen
-        dy = sin(angle) * dlen
-        points.append((p1[0] + dx, p1[1] + dy))
+    if closed:
+        dist = ((points[-1][0] - points[0][0])**2 +
+                (points[-1][1] - points[0][1])**2)**.5
+        numdiv = round(dist / dd)
+        if numdiv != 0:
+            dlen = dist / numdiv
+            angle = atan2(points[0][1] - points[-1][1],
+                          points[0][0] - points[-1][0])
+            for i in range(numdiv - 1):
+                p1 = points[-1]
+                dx = cos(angle) * dlen
+                dy = sin(angle) * dlen
+                points.append((p1[0] + dx, p1[1] + dy))
 
     x, y = zip(*points)
-    return (x, y)
+    return (list(x), list(y))
